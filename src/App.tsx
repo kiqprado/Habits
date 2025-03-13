8fb6530e48ede04a56ad63e4cf0110ccbedfbdd3
Kaique Prado <kiqprado@outlook.com>
2025-03-13 15:45:16 -0300

    eat: implement activity list rendering logic, checkpoints, and user-driven deletion
    - Added logic to render the activity list based on user activities
    - Introduced checkpoints for each activity index to track progress
    - Implemented logic to delete activities based on user choice
    - Improved code clarity and organization for better maintainability

--- src/App.tsx
+++ src/App.tsx
@@ -13,24 +13,65 @@ export function App() {
   const [ emojiPickerModal, setEmojiPickerModal ] = useState(false)
   const [ activityDailyEmojisList, setActivityDailyEmojisList] = useState<string[]>([])
 
-  const [ checkActivityStatus, setCheckActivityStatus ] = useState(false)
+  const [ confirmDeleteActivityEmojiModal, setConfirmDeleteActivityEmojiModal ] = useState(false)
+  const [ activityEmojiToRemove, setActivityEmojiToRemove ] = useState<string | null>(null)
+
+  const [ dailyCheckPointActivityList, setDailyCheckPointActivityList ] = useState<boolean[][]>([])
+  const [ dailyDatesCheckPoint, setDailyDatesCheckPoint ] = useState<string[]>([])
 
   const formattedDate = dayjs().format('DD/MM')
 
-  function HandleEmojiPickerModal() {
+  function ToggleEmojiPickerModal() {
     setEmojiPickerModal((prev) => !prev)
   }
 
-  function HandleActivityDailyEmojisList( emojiData: { emoji: string}) {
+  function HandleAddActivityDailyEmojisList( emojiData: { emoji: string}) {
     const activityEmoji = emojiData.emoji
 
     setActivityDailyEmojisList(prevActivityList => [...prevActivityList, activityEmoji])
 
-    HandleEmojiPickerModal()
+    ToggleEmojiPickerModal()
   }
 
-  function HandleActivityStatus() {
-    setCheckActivityStatus((prev => !prev))
+  function ToggleActivityDeleteEmojiModal() {
+    setConfirmDeleteActivityEmojiModal((prev) => !prev)
+  }
+
+  function HandleSelectedActivityEmojiToDelete( activityEmoji: string) {
+    setActivityEmojiToRemove(activityEmoji)
+    ToggleActivityDeleteEmojiModal()
+  }
+
+  function HandleDeleteActivityEmoji() {
+    setActivityDailyEmojisList(prevActivityList => prevActivityList.filter(activityEmoji => activityEmoji !== activityEmojiToRemove))
+
+    setActivityEmojiToRemove(null)
+    ToggleActivityDeleteEmojiModal()
+  }
+
+  function HandleAddNewDailyActivityListCheckPoint() {
+    if(activityDailyEmojisList.length === 0 ) {
+      return
+    }
+
+    setDailyDatesCheckPoint(prevCheckList => [...prevCheckList, formattedDate])
+
+    setDailyCheckPointActivityList(prevCheckListActivities => [
+      ...prevCheckListActivities,
+      activityDailyEmojisList.map(() => false)
+    ])
+  }
+
+  function HandleCheckActivityStatus(dayIndex: number, activityIndex: number) {
+    setDailyCheckPointActivityList(prevActivityList =>
+      prevActivityList.map((dayCheckPoint, i) =>
+        i === dayIndex ? 
+          dayCheckPoint.map((checked, index) =>
+          index === activityIndex ? !checked : checked
+        ) 
+        : dayCheckPoint
+      )
+    )
   }
 
   return (
@@ -41,14 +82,15 @@ export function App() {
 
           <div className='space-y-3'>
             <button 
-              onClick={HandleEmojiPickerModal}
+              onClick={ToggleEmojiPickerModal}
               className='flex items-center gap-2 px-3 py-1.5 font-bold border rounded-xl border-purple-600 hover:border-purple-400 text-purple-400 hover:text-purple-200'
             >
               <Plus/>
               Atividade
             </button>
 
-            <button 
+            <button
+              onClick={HandleAddNewDailyActivityListCheckPoint} 
               className='flex items-center gap-2 px-3 py-1.5 font-bold border rounded-xl border-purple-600 hover:border-purple-400 text-purple-400 hover:text-purple-200'
             >
               <Plus/>
@@ -63,6 +105,7 @@ export function App() {
               return (
                 <button
                   key={index}
+                  onClick={() => HandleSelectedActivityEmojiToDelete(activity)}
                   className='flex items-center text-4xl'
                 >
                   {activity}
@@ -71,34 +114,69 @@ export function App() {
             })}
           </div>
 
-          <div className='flex flex-col gap-4 items-center'>
-            <span className='font-medium text-2xl text-purple-200 hover:text-purple-100'>
-              {formattedDate}
-            </span>
+          { dailyCheckPointActivityList.map((checkpoints, dayIndex) => {
+            return (
+              <div
+                key={dayIndex} 
+                className='flex flex-col gap-4 items-center'
+              >
+                <span className='font-medium text-2xl text-purple-200 hover:text-purple-100'>
+                  {dailyDatesCheckPoint[dayIndex]}
+                </span>
 
-            <button
-              onClick={HandleActivityStatus}
-              className={`h-12 w-12  border rounded-xl border-neutral-500 hover:border-neutral-400 
-              ${checkActivityStatus ? 'bg-purple-600 border-purple-400 hover:bg-purple-400 hover:border-pink-200' : ''} `}
-            >
-            </button>
-          </div>
+                { checkpoints.map((checked, index) => {
+                  return (
+                    <button
+                      key={index}
+                      onClick={() => HandleCheckActivityStatus(dayIndex, index)}
+                      className={`h-12 w-12  border rounded-xl border-neutral-500 hover:border-neutral-400 
+                      ${checked ? 'bg-purple-600 border-purple-400 hover:bg-purple-400 hover:border-pink-200' : ''} `}
+                    >
+                    </button>
+                  )
+                }) }
+              </div>
+            )
+          })}
         </div>
 
         { emojiPickerModal && (
           <div className='fixed inset-0 flex bg-neutral-950/50'>
             <div className='m-auto bg-neutral-900 flex flex-col items-end'>
               <button
-                onClick={HandleEmojiPickerModal} 
+                onClick={ToggleEmojiPickerModal} 
                 className='text-purple-400 hover:text-purple-200'
               >
                 <X/>
               </button>
 
               <EmojiPicker
-                onEmojiClick={HandleActivityDailyEmojisList}
+                onEmojiClick={HandleAddActivityDailyEmojisList}
                 theme='dark'
               />
+            </div>
+          </div>
+        )}
+
+        { confirmDeleteActivityEmojiModal && (
+          <div className='fixed inset-0 flex bg-neutral-950/50'>
+            <div className='m-auto flex flex-col items-center gap-3 px-3 py-1.5 rounded-xl bg-neutral-700'>
+              <span className='text-purple-300 font-bold'>Deseja excluir a atividade?</span>
+
+              <div className='flex items-center gap-3'>
+                <button
+                  onClick={HandleDeleteActivityEmoji}
+                  className='flex items-center gap-2 px-3 py-1.5 font-bold border rounded-xl border-purple-600 hover:border-purple-400 text-purple-400 hover:text-purple-200'
+                >
+                  Confirmar
+                </button>
+                <button
+                  onClick={ToggleActivityDeleteEmojiModal}
+                  className='flex items-center gap-2 px-3 py-1.5 font-bold border rounded-xl border-purple-600 hover:border-purple-400 text-purple-400 hover:text-purple-200'
+                >
+                  Cancelar
+                </button>
+              </div>
             </div>
           </div>
         )}
